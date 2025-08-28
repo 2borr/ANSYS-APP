e590a98eeb34a2e6b2f471597aed83115f0aeef8
2borr <odusola.tunbosun@gmail.com>
2025-08-28 16:58:36 -0400

    Update main.tf with VPC

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = var.region
 }
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "ecs-vpc"
+  }
+}
