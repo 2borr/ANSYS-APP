f810c551355cbd4016040a8b537fd979aee35be4
2borr <odusola.tunbosun@gmail.com>
2025-08-28 16:59:44 -0400

    Update main.tf with Public subnets

--- main.tf
+++ main.tf
@@ -10,3 +10,33 @@ resource "aws_vpc" "vpc" {
     Name = "ecs-vpc"
   }
 }
+
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public_subnet_a_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = var.availability_zone_a
+  tags = {
+    Name = "ecs-public-subnet-a"
+  }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public_subnet_b_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = var.availability_zone_b
+  tags = {
+    Name = "ecs-public-subnet-b"
+  }
+}
+
+resource "aws_subnet" "public_c" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public_subnet_c_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = var.availability_zone_c
+  tags = {
+    Name = "ecs-public-subnet-c"
+  }
+}
